1ea1a43f08b9e553ca75ea083eb98218a26a3785
Stefano Brilli <stefanobrilli@gmail.com>
2015-04-24 15:36:35 +0200

    Add everything :)

---
+++ src/target_blank.js
@@ -0,0 +1,29 @@
+//
+//  Target blank extension
+//  [somelink](https://some.url.com/some/path) -> <a target="_blank" href="..." >somelink</a>
+//
+
+(function () {
+  'use strict';
+
+  var targetblank = function () {
+    return [
+      {
+        regex:   '<a(.*)a>',
+        replace: function (match, content) {
+          return '<a target="_blank"' + content + 'a>';
+        }
+      }
+    ];
+  };
+
+  // Client-side export
+  if (typeof window !== 'undefined' && window.Showdown && window.Showdown.extensions) {
+    window.Showdown.extensions.targetblank = targetblank;
+  }
+  // Server-side export
+  if (typeof module !== 'undefined') {
+    module.exports = targetblank;
+  }
+
+}());
---
+++ test/cases/basic.md
@@ -0,0 +1,3 @@
+Testing of the target blank extension.
+
+Link to [Google](http://google.com)
