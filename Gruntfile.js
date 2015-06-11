61bafb7fed36a2df2fcf56aba1a6e6de24ba6352
Ryan Mitchell <ryan@napaswdev.com>
2015-06-11 17:06:19 -0400

    Include Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,8 +7,7 @@ module.exports = function (grunt) {
     pkg: grunt.file.readJSON('package.json'),
     concat: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
-        sourceMap: true
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
       },
       dist: {
         src: ['src/*.js'],
